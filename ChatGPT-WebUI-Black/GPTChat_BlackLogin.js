48250a68b352de3a545c1a282e96b4805d0847bd
livelylion <92125893+livelylion@users.noreply.github.com>
2022-12-22 01:10:06 +0100

    [Feature] Use black background on the login page

---
+++ ChatGPT-WebUI-Black/GPTChat_BlackLogin.js
@@ -0,0 +1,6 @@
+const elements = document.getElementsByClassName("w-full h-full flex justify-center items-center flex-col bg-gray-50 dark:bg-gray-800");
+
+for(let x = 0; x < elements.length; x++)
+{
+    elements[x].style.backgroundColor = "black";
+}
